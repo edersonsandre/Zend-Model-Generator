5342ca3c32d8d491b4a657777b866f18af0cfebd
Laurynas Karvelis <laurynas.karvelis@gmail.com>
2012-04-11 20:02:14 +0100

    Re-organised project files so it would be easier to submodule this project in ZF project

---
+++ model-generator.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author Laurynas Karvelis <laurynas.karvelis@gmail.com>
+ * @author Explosive Brains Limited
+ * @license http://sam.zoy.org/wtfpl/COPYING
+ */
+
+header('Content-type: text/plain; charset=utf-8');
+@ob_end_flush();
+
+// always use this dir as the main one
+chdir(__DIR__);
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    get_include_path(),
+    './library',
+
+    // add path to ZF library/ directory if needed
+    '/www/ZendFramework-1.11.11-minimal/library'
+)));
+
+if(!class_exists('Zend_Loader_Autoloader', false)) {
+    require_once 'Zend/Loader/Autoloader.php';
+}
+
+Zend_Loader_Autoloader::getInstance()
+    ->registerNamespace('ModelGenerator');
+
+// prepare paths
+define('APPLICATION_PATH', realpath('./../application'));
+
+$configIniLocation = __DIR__ . '/config.ini';
+$applicationIniLocation = APPLICATION_PATH . '/configs/application.ini';
+
+// init our generator
+$generator = new ModelGenerator_Generator($configIniLocation, $applicationIniLocation);
+
+// generate models :)
+$generator->run();
